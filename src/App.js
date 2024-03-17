ccc09514bafb315c90beb4969e54ed2148aa4228
Siva Dinesh k <ksd.ccbp4@gmail.com>
2024-03-17 18:19:48 +0000

    removed all the confusing code

--- src/App.js
+++ src/App.js
@@ -1,39 +1,11 @@
-import {Component} from 'react'
-import {Route, Switch, Redirect} from 'react-router-dom'
+// import {Component} from 'react'
+// import {Route, Switch, Redirect} from 'react-router-dom'
 
 import './App.css'
-import ThemeContext from './context/ThemeContext'
-import ProtectedRoute from './components/ProtectedRoute'
-import Login from './components/Login'
-import Header from './components/HeaderComponent/Header'
-import NotFoundPage from './components/NotFoundPage'
-import FailureView from './components/FailureViews/FailureView'
 
 // Replace your code here
 // TODO: add pop-ups to Navbar{ menu and logout btn/icon}.
 // TODO: add Retry method
-class App extends Component {
-  state = {isDark: true}
-
-  toggleTheme = () => {
-    this.setState(prevState => ({isDark: !prevState.isDark}))
-  }
-
-  render() {
-    const {isDark} = this.state
-
-    return (
-      <ThemeContext.Provider value={{isDark, toggleTheme: this.toggleTheme}}>
-        <Switch>
-          <Route exact path="/login" component={Login} />
-          <ProtectedRoute exact path="/" component={Header} />
-          <ProtectedRoute path="/not-found" component={NotFoundPage} />
-          <ProtectedRoute path="/no-vid" component={FailureView} />
-          <Redirect to="/not-found" />
-        </Switch>
-      </ThemeContext.Provider>
-    )
-  }
-}
+const App = () => <div> hello !! redo the code </div>
 
 export default App
