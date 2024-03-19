1f07c37fcc57586d6f2fc59640a613083a9b039d
Siva Dinesh k <ksd.ccbp4@gmail.com>
2024-03-19 16:27:00 +0000

    context filtered

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -3,12 +3,9 @@ import React from 'react'
 const AppContext = React.createContext({
   isDark: false,
   changeTheme: () => {},
-  activeTab: 'Home',
   savedVideosList: [],
-  clickedRetry: () => {},
   removeSavedVideo: () => {},
   addSavedVideo: () => {},
-  changeActiveTab: () => {},
 })
 
 export default AppContext
